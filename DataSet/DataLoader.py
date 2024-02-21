7ffa3b04e774356a5e0d29c401b743cebc77c6b8
Jiaxin Yin <157468505+JXYin24@users.noreply.github.com>
2024-02-21 15:31:59 +0800

    Update DataLoader.py

--- DataSet/DataLoader.py
+++ DataSet/DataLoader.py
@@ -1,20 +1,11 @@
 from torch.utils.data import DataLoader
-from DataSet.MyDataset import KDDdataset, KDD_train_test_split, CsvDataset, MatDataset, NpzDataset
+from DataSet.MyDataset import CsvDataset, MatDataset, NpzDataset
 
 
 def get_dataloader(model_config: dict):
     dataset_name = model_config['dataset_name']
-    if dataset_name == 'KDD':
-        train_data, train_label, test_data, test_label = KDD_train_test_split(model_config['data_dir'])
-        train_set = KDDdataset(train_data, train_label)
-        test_set = KDDdataset(test_data, test_label)
 
-    elif dataset_name == 'KDDrev':
-        train_data, train_label, test_data, test_label = KDD_train_test_split(model_config['data_dir'], rev=True)
-        train_set = KDDdataset(train_data, train_label)
-        test_set = KDDdataset(test_data, test_label)
-
-    elif dataset_name in ['thyroid', 'arrhythmia', 'lympho', 'mnist', 'wbc', 'satimage-2', 'breastw', 'shuttle', 'pima', 'cardio', 'ionosphere', 'mammography','glass','musk','letter','vowels','satellite','shuttle']:
+    if dataset_name in ['thyroid', 'arrhythmia', 'lympho', 'mnist', 'wbc', 'satimage-2', 'breastw', 'shuttle', 'pima', 'cardio', 'ionosphere', 'mammography','glass','musk','letter','vowels','satellite','shuttle']:
         train_set = MatDataset(dataset_name, model_config['data_dim'], model_config['data_dir'], mode='train')
         test_set = MatDataset(dataset_name, model_config['data_dim'], model_config['data_dir'], mode='eval')
 
